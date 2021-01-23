be2d861c2000e06b7d11ddeb624e46b41ef5a8d5
Lennard Scheibel <contact@lennardscheibel.de>
2021-01-23 02:57:25 +0100

    Fine tune animations

--- Assets/Character/MainCharacterController.cs
+++ Assets/Character/MainCharacterController.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
@@ -12,14 +9,24 @@ public class MainCharacterController : MonoBehaviour
     private Vector3 _forward, _right;
     private MousePositionInWorld _mousePosition;
     
-    private CharacterController _controller;
+    private CharacterController _characterController;
 
     [SerializeField] private float movementSpeed = 4f;
-    [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float jumpHeight = 0.2f;
 
+    [SerializeField] private float gravity = 2f;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float jumpSpeed = 0.5f;
+
     private float _directionY;
-    
+
+    // private Vector3 _moveDirection;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
     void Start()
     {
         _forward = Camera.main.transform.forward;
@@ -27,9 +34,7 @@ public class MainCharacterController : MonoBehaviour
         _forward = Vector3.Normalize(_forward);
         _right = Quaternion.Euler(new Vector3(0, 90, 0)) * _forward;
 
-        _mousePosition = gameObject.AddComponent<MousePositionInWorld>();
-
-        _controller = gameObject.GetComponent<CharacterController>();
+        _mousePosition = gameObject.GetComponent<MousePositionInWorld>();
         _animator = gameObject.GetComponent<Animator>();
     }
 
@@ -39,27 +44,51 @@ public class MainCharacterController : MonoBehaviour
         WatchVelocity();
     }
 
+    // private void FixedUpdate()
+    // {
+    //     float horizontal = Input.GetAxis("Horizontal");
+    //     float vertical = Input.GetAxis("Vertical");
+    //
+    //     Vector3 inputDirection = _forward * vertical + _right * horizontal;
+    //
+    //     Vector3 flatMovement = moveSpeed * Time.deltaTime * inputDirection;
+    //
+    //     _moveDirection = new Vector3(flatMovement.x, _moveDirection.y, flatMovement.z);
+    //
+    //     if (PlayerJumped)
+    //         _moveDirection.y = jumpSpeed;
+    //     else if (_characterController.isGrounded)
+    //         _moveDirection.y = 0f;
+    //     else
+    //         _moveDirection.y -= gravity * Time.deltaTime;
+    //
+    //     _characterController.Move(_moveDirection);
+    // }
+
+    private bool PlayerJumped => _characterController.isGrounded && Input.GetKey(KeyCode.Space);
+
+
     void Move()
     {
         float hInput = Input.GetAxis("Horizontal");
         float vInput = Input.GetAxis("Vertical");
-
+    
         Vector3 direction = _forward * vInput + _right * hInput;
-
-        if (_controller.isGrounded)
+    
+        if (_characterController.isGrounded)
         {
             if (Input.GetButton("Jump"))
             {
                 _directionY = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
             }
         }
-
+    
         _directionY += gravity * Time.deltaTime;
-
+    
         direction.y = _directionY;
+    
+        var result = _characterController.Move(direction * (movementSpeed * Time.deltaTime));
 
-        _controller.Move(direction * (movementSpeed * Time.deltaTime));
-        
         if (direction != Vector3.zero)
         {
             Vector3 mPos = _mousePosition.worldPosition;
@@ -69,7 +98,7 @@ public class MainCharacterController : MonoBehaviour
 
     void WatchVelocity()
     {
-        Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal") * -1, 0, Input.GetAxis("Vertical")).normalized;
+        Vector3 inputDirection = new Vector3(Input.GetAxis("Horizontal") * -1, 0, Input.GetAxis("Vertical"));
         
         Quaternion camRot = Quaternion.LookRotation(_forward);
         Vector3 lookDirection = camRot * ((_mousePosition.worldPosition - transform.position)).normalized;
@@ -81,7 +110,7 @@ public class MainCharacterController : MonoBehaviour
         Debug.DrawLine(transform.position + Vector3.up, lookDirection);
         Debug.DrawLine(Vector3.up, rotatedInput);
         
-        _animator.SetFloat("velocityRight", rotatedInput.z);
-        _animator.SetFloat("velocityForward", rotatedInput.x);
+        _animator.SetFloat("velocityRight", rotatedInput.z, 0.1f, Time.deltaTime);
+        _animator.SetFloat("velocityForward", rotatedInput.x, 0.1f, Time.deltaTime);
     }
 }
--- Assets/MousePositionInWorld.cs
+++ Assets/MousePositionInWorld.cs
@@ -1,18 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MousePositionInWorld : MonoBehaviour
 {
+    private Plane _plane = new Plane(Vector3.up, 0);
+
+    [SerializeField] private Transform offset;
+    
     public Vector3 worldPosition;
     
+
     void Update()
     {
-        Plane plane = new Plane(Vector3.up, 0);
+        _plane.distance = offset.position.y;
 
         float distance;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (plane.Raycast(ray, out distance))
+        if (_plane.Raycast(ray, out distance))
         {
             worldPosition = ray.GetPoint(distance);
         }
