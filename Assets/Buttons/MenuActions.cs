832879b7473a722c5c7704803d6a07b28a13cad9
Julius Dockwarder <hello@julius.dev>
2021-01-27 10:53:28 +0100

    feat: better scene management

--- Assets/Buttons/MenuActions.cs
+++ Assets/Buttons/MenuActions.cs
@@ -7,7 +7,7 @@ public class MenuActions : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        gameObject.SetActive(false);
     }
 
     public void QuitGame() {
